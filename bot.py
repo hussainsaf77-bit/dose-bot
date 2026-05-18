38112c73a4cc767947c451be5db88dd9a3184577
hussainsaf77-bit <hussainsaf77@gmail.com>
2026-05-18 16:33:24 +0300

    fix sugar_ pattern in main_cb

--- bot.py
+++ bot.py
@@ -4150,7 +4150,8 @@ def build_conv():
                 CallbackQueryHandler(handle_m_bp, pattern="^m_bp$"),
                 CallbackQueryHandler(sugar_menu, pattern="^m_sugar$"),
                 CallbackQueryHandler(sugar_handler, pattern="^sugar_"),
-                CallbackQueryHandler(main_cb, pattern="^(m_|do_lang|do_country|change_lang|pay_|cal_|act_|dis_|sugar_)"),
+                CallbackQueryHandler(main_cb, pattern="^(m_|do_lang|do_country|change_lang|pay_|cal_|act_|dis_)"),
+                CallbackQueryHandler(sugar_handler, pattern="^sugar_"),
                 CallbackQueryHandler(manual_drug_input, pattern="^manual_input$")],
             STATE_BMI_WEIGHT: [
                 CallbackQueryHandler(bmi_cb, pattern="^bmi_"),
